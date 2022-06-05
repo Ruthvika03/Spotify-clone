1322d88016eb7b68f9c23ab7d22b4578fe6a2378
Ruthvika <ruthvikareddy16@gmail.com>
2022-06-05 15:21:52 +0530

    First Commit

--- src/spotify.js
+++ src/spotify.js
@@ -15,17 +15,6 @@ const scopes = [
 "user-top-read",
 "user-modify-playback-state",];
 
-/*export const getTokenFromResponse = () => {
-    return window.location.hash
-      .substring(1)
-      .split("&")
-      .reduce((initial, item) => {
-        var parts = item.split("=");
-        initial[parts[0]] = decodeURIComponent(parts[1]);
-  
-        return initial;
-      }, {});
-  };*/
   
   export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
