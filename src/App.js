88a9f9ae492a95c0333ac01b1b0ea0acd5c16e94
Ruthvika <ruthvikareddy16@gmail.com>
2022-10-30 18:30:38 +0530

    Connecting spotify with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
+const spotify = new SpotifyWebApi();
 function App() {
+  const [token, setToken] = useState(null);
+
+  // Run code based on given condition.
+  useEffect(() => {
+     const hash = getTokenFromUrl();
+     window.location.hash = ""; 
+     const _token = hash.access_token;
+
+     if(_token){
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      
+      // Allows back n forth btw react n spotify
+
+      spotify.getMe().then(user => {
+        console.log('person', user);
+      })
+     }
+
+     console.log("I HAVE A TOKEN :point ", token)
+  }, []);// Used to keep an eye on the url
+
   return (
     <div className="app">
-      
+      {
+        token ? (
+          <h1> I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     <Login />
       
     </div>
   );
-}
+} 
 
 export default App;
--- src/spotify.js
+++ src/spotify.js
@@ -2,9 +2,9 @@
 // documentation/web-playback-sdk/quick-start/#
 
 export const authEndpoint = 
-"https://accounts.spotify.com/authorize";
+"https://accounts.spotify.com/authorize"; // Redirects to spotify page when login button is clicked.
 
-const redirectUri = "https://localhost:3000/";
+const redirectUri = "http://localhost:3000/"; // Redirects to home page once logged in.
 
 const clientId = "3169bd1fc6cc415e9bcb03f269a4dede";
 
@@ -14,6 +14,20 @@ const scopes = [
 "user-read-playback-state",
 "user-top-read",
 "user-modify-playback-state",];
+
+export const getTokenFromUrl = () => {
+   return window.location.hash
+   .substring(1) // chopping the string
+   .split("&")
+   .reduce((initial, item) => {
+
+    var parts = item.split("=");
+    initial[parts[0]] = decodeURIComponent(parts[1]) ;
+
+    return initial;
+   }, {});
+
+} //grabs the accessToken from url.
 
   
   export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
