d75330b9cbf8d302b786275cbfba9748c0b5ad70
Ruthvika <ruthvikareddy16@gmail.com>
2022-11-02 01:27:00 +0530

    Added sidebar,footer,body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 function App() {
-  const [token, setToken] = useState(null);
+  //const [token, setToken] = useState(null);
+  const [{user,token}, dispatch] = useDataLayerValue();
 
   // Run code based on given condition.
   useEffect(() => {
@@ -15,33 +19,35 @@ function App() {
      const _token = hash.access_token;
 
      if(_token){
-      setToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+     // setToken(_token);
 
       spotify.setAccessToken(_token);
-      
+
       // Allows back n forth btw react n spotify
 
       spotify.getMe().then(user => {
-        console.log('person', user);
-      })
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+       
+      });
      }
 
      console.log("I HAVE A TOKEN :point ", token)
   }, []);// Used to keep an eye on the url
 
-  return (
-    <div className="app">
-      {
-        token ? (
-          <h1> I am logged in</h1>
-        ) : (
-          <Login />
-        )
-      }
-    <Login />
-      
-    </div>
-  );
-} 
+  console.log('person', user);
+  console.log('alien', token);
+
+  return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
+  
+}
+
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: 'BQDEPwAEp4bjFa__Ua9O9kPzvt8jo3Xc0BdU7p6jqGUQszPc2AqAfmaoIOjjawCSOukitY0c6YXcanWqUcNu8rA5w7aDb-02uq7qCIO8XuPfCmxIb2sg9SpweyEIefPZjhN3XHRtbhYLr-zXjjoZHpkw1T__IBfmUSp3sUOFn3b6uwlTl3eKM7JSu_rO2WHS9DmG700AOkXDSxXn',
+};
+
+const reducer = (state, action) => {
+console.log(action);
+//Action -> type, [payload]
+  switch(action.type){
+    case 'SET_USER':
+        return{
+            ...state,
+            user: action.user,
+        } 
+        case 'SET_TOKEN' :
+          return {
+            ...state,
+            token: action.token,
+          }
+        default:
+            return state;
+  }
+}
+
+export default reducer;
